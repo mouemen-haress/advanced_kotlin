ab0c1229046987b0b0a4646a4ebb1f9c751f97c9
mouemen.haress.1998@gmail.com <mouemen.haress.1998@gmail.com>
2023-05-02 02:05:08 +0300

    first fragment cycle comment

--- app/src/main/java/com/example/twowaydemo1/MainActivity.kt
+++ app/src/main/java/com/example/twowaydemo1/MainActivity.kt
@@ -7,13 +7,8 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.twowaydemo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mBinding: ActivityMainBinding
-    private lateinit var mViewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        mViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
-        mBinding.viewModel = mViewModel
-        mBinding.lifecycleOwner = this
     }
 }
