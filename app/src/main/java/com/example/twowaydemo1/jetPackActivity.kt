1172da38c30df305685facc2685f4eee58048ff8
mouemen.haress.1998@gmail.com <mouemen.haress.1998@gmail.com>
2023-05-27 22:08:49 +0300

    box component

--- app/src/main/java/com/example/twowaydemo1/jetPackActivity.kt
+++ app/src/main/java/com/example/twowaydemo1/jetPackActivity.kt
@@ -3,16 +3,17 @@ package com.example.twowaydemo1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,67 +25,150 @@ class jetPackActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            Columntutorial("Android")
-            rowtutorial("Android")
+            BoxExampleThree()
         }
 
     }
 }
 
-
 @Composable
-fun rowtutorial(name: String) {
-    Row(
+fun BoxExampleOne() {
+    Box(
         modifier = Modifier
-            .background(color = Color.Green)
-            .fillMaxSize(),
-        verticalAlignment = Alignment.Bottom,
-        horizontalArrangement = Arrangement.SpaceEvenly
+            .background(Color.Gray)
+            .size(180.dp, 300.dp)
     ) {
-        sub("AB")
-        sub("CD")
-        sub("EF")
-    }
+        Box(
+            modifier = Modifier
+                .background(Color.Yellow)
+                .size(60.dp, 60.dp)
+        ) {
+        }
+        Text(
+            text = "hello",
+            style = MaterialTheme.typography.bodyLarge,
 
+            modifier = Modifier
+                .background(Color.White)
+                .size(90.dp, 50.dp)
+                .align(Alignment.BottomCenter),
 
-}
-
-@Composable
-fun sub(name: String) {
-    Text(
-        text = name,
-        fontSize = 16.sp,
-        fontWeight = FontWeight.Bold,
-        color = Color.Blue,
-        textAlign = TextAlign.Center,
-        modifier = Modifier
-            .background(color = Color.Yellow)
-            .border(2.dp, color = Color.Blue)
-            .padding(10.dp)
-    )
-
-}
-
-@Composable
-fun Columntutorial(name: String) {
-    Column(
-        modifier = Modifier
-            .background(color = Color.Green)
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        sub("slall")
-        sub(name)
-        sub(name)
+            )
     }
 }
+
+@Composable
+fun BoxExampleTwo() {
+    Box(
+        modifier = Modifier
+            .background(Color.Gray)
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "top start",
+            style = MaterialTheme.typography.bodyLarge,
+
+            modifier = Modifier
+                .background(Color.White)
+                .size(120.dp, 50.dp)
+                .padding(10.dp)
+                .align(Alignment.TopStart)
+
+        )
+
+        Text(
+            text = "top end",
+            style = MaterialTheme.typography.bodyLarge,
+
+            modifier = Modifier
+                .background(Color.White)
+                .size(120.dp, 50.dp)
+                .padding(10.dp)
+                .align(Alignment.TopEnd)
+
+        )
+
+
+
+        Text(
+            text = "Center",
+            style = MaterialTheme.typography.bodyLarge,
+
+            modifier = Modifier
+                .background(Color.White)
+                .size(120.dp, 50.dp)
+                .padding(10.dp)
+                .align(Alignment.Center)
+
+        )
+
+
+        Text(
+            text = "CenterEnd",
+            style = MaterialTheme.typography.bodyLarge,
+
+            modifier = Modifier
+                .background(Color.White)
+                .size(120.dp, 50.dp)
+                .padding(10.dp)
+                .align(Alignment.CenterEnd)
+
+        )
+
+        Text(
+            text = "BottomStart",
+            style = MaterialTheme.typography.bodyLarge,
+
+            modifier = Modifier
+                .background(Color.White)
+                .size(120.dp, 50.dp)
+                .padding(10.dp)
+                .align(Alignment.BottomStart)
+
+        )
+
+        Text(
+            text = "BottomCenter",
+            style = MaterialTheme.typography.bodyLarge,
+
+            modifier = Modifier
+                .background(Color.White)
+                .size(120.dp, 50.dp)
+                .padding(10.dp)
+                .align(Alignment.BottomCenter)
+
+        )
+    }
+}
+
+@Composable
+fun BoxExampleThree() {
+    Box {
+        Image(
+            painter = painterResource(id = R.drawable.test),
+            contentDescription = "test description "
+        )
+        Text(
+            text = "dec",
+            style = MaterialTheme.typography.headlineLarge,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.BottomCenter)
+        )
+
+        Button(
+            onClick = {},
+        ) {
+            Text(text = "click me ")
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TwoWayDemo1Theme {
 //        Columntutorial("sami")
-        rowtutorial(name = "Android")
+        BoxExampleThree()
     }
 }
