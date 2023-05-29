9aae517e20c4356742c3919cc7ac213c4da8a4a3
mouemen <mouemen.hares@vianeos.com>
2023-05-29 17:51:38 +0300

    begin by lazyColumn

--- app/src/main/java/com/example/twowaydemo1/jetPackActivity.kt
+++ app/src/main/java/com/example/twowaydemo1/jetPackActivity.kt
@@ -1,15 +1,14 @@
 package com.example.twowaydemo1
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material.icons.Icons
@@ -34,7 +33,7 @@ class jetPackActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            RecyclerView()
         }
 
     }
@@ -43,93 +42,38 @@ class jetPackActivity : ComponentActivity() {
 }
 
 @Composable
-fun ButtonDemo() {
-    val context = LocalContext.current
+fun RecycleDemo() {
+    // like listView
+    val scrolState = rememberScrollState()
 
-    Column(
-        verticalArrangement = Arrangement.SpaceEvenly,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        NormalBtn()
-        TextBtn()
-        OutLineBtn()
-        IconBtn()
-        CustomizedBtn()
+    Column(modifier = Modifier.verticalScroll(scrolState)) {
+        for (i in 1..100) {
+            Text(
+                    text = "User name $i",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(10.dp)
+            )
+            Divider(color = Color.Black, thickness = 5.dp)
+        }
+
     }
-
 
 }
 
 @Composable
-fun NormalBtn() {
-    val context = LocalContext.current
-    Button(
-        onClick = {
-            Toast.makeText(context, "click normal btn", Toast.LENGTH_LONG).show()
-        },
-        shape = RectangleShape
-    ) {
-        Text(text = "Add to cart")
-    }
-}
+fun RecyclerView() {
+    // like RecyclerView
+    LazyColumn(modifier = Modifier.fillMaxHeight()) {
+        items(100) {
 
-@Composable
-fun CustomizedBtn() {
-    val context = LocalContext.current
-    Button(
-        onClick = {
-            Toast.makeText(context, "click normal btn", Toast.LENGTH_LONG).show()
-        },
-        contentPadding = PaddingValues(16.dp),
-        border = BorderStroke(10.dp, Color.Black),
-        colors = ButtonDefaults.textButtonColors(
-            containerColor = Color.DarkGray,
-            contentColor = Color.White
-        ),
-        shape = RectangleShape
-    ) {
-        Text(
-            text = "Add to cart",
-            style = MaterialTheme.typography.displaySmall,
-            modifier = Modifier.padding(5.dp)
-        )
-    }
-}
+            Text(
+                    text = "User name $it",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(10.dp)
+            )
+            Divider(color = Color.Black, thickness = 5.dp)
 
-@Composable
-fun TextBtn() {
-    val context = LocalContext.current
-    TextButton(onClick = {
-        Toast.makeText(context, "hi baby", Toast.LENGTH_LONG).show()
-    }) {
-        Text(text = "Add to cart")
-    }
-}
-
-@Composable
-fun OutLineBtn() {
-    val context = LocalContext.current
-    OutlinedButton(onClick = {
-        Toast.makeText(context, "hi baby", Toast.LENGTH_LONG).show()
-    }) {
-        Text(text = "Add to cart")
-    }
-}
-
-@Composable
-fun IconBtn() {
-    val context = LocalContext.current
-    IconButton(onClick = {
-        Toast.makeText(context, "click normal btn", Toast.LENGTH_LONG).show()
-    }) {
-        Icon(
-            Icons.Filled.Phone,
-            contentDescription = "dec",
-            tint = Color.Blue,
-            modifier = Modifier.size(70.dp)
-        )
+        }
     }
 }
 
@@ -137,6 +81,6 @@ fun IconBtn() {
 @Composable
 fun DefaultPreview() {
     TwoWayDemo1Theme {
-        ButtonDemo()
+        RecycleDemo()
     }
 }
