b9753550537df615617b27cc60b0ee9a30022f3a
mouemen <mouemen.hares@vianeos.com>
2023-05-30 17:55:24 +0300

    advanced recyclerView

--- app/src/main/java/com/example/twowaydemo1/jetPackActivity.kt
+++ app/src/main/java/com/example/twowaydemo1/jetPackActivity.kt
@@ -10,15 +10,19 @@ import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -27,86 +31,57 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.composerecyclerview.model.TvShow
 import com.example.twowaydemo1.ui.theme.TwoWayDemo1Theme
 
 class jetPackActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RecyclerView1 {
-                Toast.makeText(this, it, Toast.LENGTH_LONG).show()
-            }
+
         }
 
     }
 
 
 }
+
 
 
 @Composable
-fun RecyclerView1(selectedItem: (String) -> Unit) {
-    // like RecyclerView
-    LazyColumn(modifier = Modifier.fillMaxHeight()) {
-        items(100) {
-            Text(
-                text = "User name $it",
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier
-                    .padding(10.dp)
-                    .clickable { selectedItem("$it selected") }
-            )
-            Divider(color = Color.Black, thickness = 5.dp)
+public fun tvShowListItem(tvShow: TvShow) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor =
+            if (isSelected) MaterialTheme.colorScheme.primaryContainer
+            else
+                MaterialTheme.colorScheme.surfaceVariant)
+    ) {
 
-        }
     }
+
 }
 
 @Composable
-fun RecyclerView() {
-    // like RecyclerView
-    LazyColumn(modifier = Modifier.fillMaxHeight()) {
-        items(100) {
-
-            Text(
-                text = "User name $it",
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier.padding(10.dp)
-            )
-            Divider(color = Color.Black, thickness = 5.dp)
-
-        }
-    }
+private fun TvShowImage(tvShow: TvShow) {
+    Image(
+        painter = painterResource(id = tvShow.imageId),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .padding(4.dp)
+            .height(140.dp)
+            .width(100.dp)
+            .clip(RoundedCornerShape(corner = CornerSize(10.dp)))
+    )
 }
 
-
-@Composable
-fun ListDemo() {
-    // like listView
-    val scrolState = rememberScrollState()
-
-    Column(modifier = Modifier.verticalScroll(scrolState)) {
-        for (i in 1..100) {
-            Text(
-                text = "User name $i",
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier.padding(10.dp)
-            )
-            Divider(color = Color.Black, thickness = 5.dp)
-        }
-
-    }
-
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     var c = LocalContext.current
     TwoWayDemo1Theme {
-        RecyclerView1 {
-            Toast.makeText(c, it, Toast.LENGTH_LONG).show()
 
-        }
     }
 }
