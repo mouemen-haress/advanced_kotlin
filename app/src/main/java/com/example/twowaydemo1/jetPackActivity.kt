e8175e262b47491b35156c6b0452c9d5d2c22a35
alaadairy <alaa.dairy@vianeos.com>
2023-05-31 17:25:37 +0300

    side effect

--- app/src/main/java/com/example/twowaydemo1/jetPackActivity.kt
+++ app/src/main/java/com/example/twowaydemo1/jetPackActivity.kt
@@ -1,5 +1,6 @@
 package com.example.twowaydemo1
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -14,6 +15,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.*
@@ -31,7 +33,9 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,63 +46,77 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 
 import com.example.twowaydemo1.ui.theme.TwoWayDemo1Theme
 
-class jetPackActivity : ComponentActivity() {/*
-    *       <State Hosting design Pattern> Unidirectional Data flow
-    *We are sending state from caller to the composable.
-    * And we are sending event from composable to the caller.
-    * */
+class jetPackActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel = viewModel<MyViewModel>()
-
             TwoWayDemo1Theme() {
-                Column(
+                // A surface container using the 'background' color from the theme
+                Surface(
                     modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    // state hosting design pattern
-                    val count = viewModel.count
-                    MyButton(count, {
-                        viewModel.increaseCount()
-                    })
+                    MainScreen()
                 }
             }
-
-
         }
-
     }
-
-
 }
 
-
-//val count = mutableStateOf(0)
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyButton(currentCount: Int, updateCount: (Int) -> Unit) {
-    val context = LocalContext.current
-    Button(
-        onClick = {
-            updateCount(currentCount)
-        },
-        contentPadding = PaddingValues(16.dp),
-        border = BorderStroke(10.dp, Color.Black),
-        colors = ButtonDefaults.textButtonColors(
-            containerColor = Color.DarkGray, contentColor = Color.White
-        ),
-        shape = RectangleShape
+fun MainScreen(
+    modifier: Modifier = Modifier,
+    context: Context = LocalContext.current
+) {
+
+    var total by remember { mutableStateOf(0.0) }
+    var input by remember { mutableStateOf("") }
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(50.dp),
+        verticalArrangement = Arrangement.SpaceEvenly
     ) {
         Text(
-            text = "${currentCount}",
-            style = MaterialTheme.typography.displaySmall,
-            modifier = Modifier.padding(5.dp)
+            modifier = modifier.fillMaxWidth(),
+            text = "$input Total is ${total.toString()}",
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            color = Color.DarkGray
+        )
+        OutlinedTextField(
+
+            modifier = modifier.fillMaxWidth(),
+            placeholder = { Text("Enter value here") },
+            value = input,
+            onValueChange = {
+                input = it
+            },
+            textStyle = TextStyle(
+                color = Color.LightGray,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            label = { Text(text = "New count") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
 
+        Button(
+            modifier = modifier.fillMaxWidth(),
+            onClick = {
+                total += input.toDouble()
+            }
+        ) {
+            Text(
+                text = "Count",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
     }
-
 
 }
