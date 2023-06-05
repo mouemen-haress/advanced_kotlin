e73fba0c86377141a201ba80d1353eb575cda2d5
mouemen <mouemen.hares@vianeos.com>
2023-06-05 17:55:34 +0300

    instructerd concurency

--- app/src/main/java/com/example/twowaydemo1/MainActivity.kt
+++ app/src/main/java/com/example/twowaydemo1/MainActivity.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
@@ -25,39 +27,12 @@ class MainActivity : AppCompatActivity() {
         btnCount = findViewById(R.id.btnCount)
         tvCount = findViewById(R.id.tvCount)
 
-        // async task inside IO THREAD
-//        CoroutineScope(IO).launch {
-//
-//            val stockOne = async { getStockOne() }
-//            val stockTwo = async { getStockTwo() }
-//
-//            val result = stockOne.await() + stockTwo.await()
-//            Log.i("MyTag", "result =  " + result)
-//
-//        }
 
-
-        // async task inside MAIN THREAD
-        CoroutineScope(Main).launch {
-
-            val stockOne = async(IO) { getStockOne() }
-            val stockTwo = async(IO) { getStockTwo() }
-
-            val result = stockOne.await() + stockTwo.await()
-            Toast.makeText(applicationContext, "rsult is : $result", Toast.LENGTH_SHORT).show()
+        btnDownloadUserData.setOnClickListener {
+            CoroutineScope(Main).launch {
+                tvCount.text = UserDataManagerOne().getTotalUserCount().toString()
+            }
         }
-
     }
 
-    private suspend fun getStockOne(): Int {
-        delay(10000)
-        Log.i("MyTag", "stock 1 returned")
-        return 55000
-    }
-
-    private suspend fun getStockTwo(): Int {
-        delay(8000)
-        Log.i("MyTag", "stock 2 returned")
-        return 35000
-    }
 }
