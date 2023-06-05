b5df245d682bda57345b283d80d6a5a00756515f
mouemen <mouemen.hares@vianeos.com>
2023-06-05 17:31:22 +0300

    async and await

--- app/src/main/java/com/example/twowaydemo1/MainActivity.kt
+++ app/src/main/java/com/example/twowaydemo1/MainActivity.kt
@@ -5,9 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
@@ -24,19 +23,29 @@ class MainActivity : AppCompatActivity() {
         btnCount = findViewById(R.id.btnCount)
         tvCount = findViewById(R.id.tvCount)
 
-        btnCount.setOnClickListener {
-            tvCount.text = count++.toString()
+        CoroutineScope(IO).launch {
+
+            val stockOne = async { getStockOne() }
+            val stockTwo = async { getStockTwo() }
+
+            val result = stockOne.await() + stockTwo.await()
+            Log.i("MyTag", "result =  " + result)
+
         }
         btnDownloadUserData.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {
-                downloadUserData()
-            }
+
         }
     }
 
-    private fun downloadUserData() {
-        for (i in 1..200000) {
-            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
-        }
+    private suspend fun getStockOne(): Int {
+        delay(10000)
+        Log.i("MyTag", "stock 1 returned")
+        return 55000
+    }
+
+    private suspend fun getStockTwo(): Int {
+        delay(8000)
+        Log.i("MyTag", "stock 2 returned")
+        return 35000
     }
 }
